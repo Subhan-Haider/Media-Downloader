9c8c3b0d8b701f6bcb752a221e828b9435920af0
Subhan-Haider <setupg98@gmail.com>
2026-06-26 14:04:17 -0400

    feat: add navigation header and footer layout with metadata to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Media Server",
   description: "Universal Media Downloader and Server",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
